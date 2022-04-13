538af26c6c92cf44fb44d6a3ef947af030053730
momo <82991184+pchm13@users.noreply.github.com>
2022-04-13 11:39:20 +0900

    TODO追加機能実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // inputタグへ入力された値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素にliタグ, buttonタグを設定
+  div.appendChild(li);
+
+  // ulタグの子要素にdiv, li, buttonタグを設定して結果を反映
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
